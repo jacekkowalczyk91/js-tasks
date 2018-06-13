3dc4eb60fc9a9d43e8affdb028061633ef9d7680
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-06-13 23:50:29 +0200

    divs

--- DatePicker/js/script.js
+++ DatePicker/js/script.js
@@ -1,13 +1,25 @@
-function Calendar(input) {
-    this.now = new Date();
-    this.day = this.now.getDate();
-    this.month = this.now.getMonth();
-    this.year = this.now.getFullYear();
+function Calendar = function() {
+...
+    this.init = function () {
+        this.divCnt = document.createElement('div');
+        this.divCnt.classList.add('calendar');
 
-    this.input = input;
-    this.divCnt = null;
-    this.divHeader = null;
-    this.divTable = null;
-    this.divDateText = null;
-    this.divButtons = null;
-}
+        this.divButtons = document.createElement('div');
+        this.divButtons.className = "calendar-prev-next";
+
+        this.divDateText = document.createElement('div');
+        this.divDateText.className = 'date-name';
+
+
+        this.divHeader = document.createElement('div');
+        this.divHeader.classList.add('calendar-header');
+
+        this.divHeader.appendChild(this.divButtons);
+        this.divHeader.appendChild(this.divDateText);
+        this.divCnt.appendChild(this.divHeader);
+
+        this.divTable = document.createElement('div');
+        this.divTable.className = 'calendar-table-cnt';
+        this.divCnt.appendChild(this.divTable);
+    };
+};
